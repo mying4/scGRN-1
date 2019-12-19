1206eabf6c42d4bfffaa229302dbb088d4b11bcf
Mufang Ying <53714288+mying4@users.noreply.github.com>
2019-12-18 21:05:14 -0600

    Delete sample_gexpr.R

--- R/sample_gexpr.R
+++
@@ -1,8 +0,0 @@
-#' Sample gene expression data
-#' 
-#' @format a data frame with 100 rows and 7 variables
-#' \describe{
-#'  \item{columns}{each column represents an observation}
-#'  \item{row}{each row represents a gene}
-#' }
-"sample_gexpr"
