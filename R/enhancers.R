ae40d82fd23b315f3889e503a954e7dee69682a5
Mufang Ying <53714288+mying4@users.noreply.github.com>
2019-12-18 21:47:53 -0600

    Add files via upload

---
+++ R/enhancers.R
@@ -0,0 +1,14 @@
+#'  Enhancer data
+#' 
+#' @format a data frame with 79056 rows and 3 variables
+#' \describe{
+#'  \item{chr}{chromosome}
+#'  \item{start}{start point of enhancer}
+#'  \item{end}{end point of enhancer}
+#' }
+#' 
+#' @source 
+#' enhancer data can be found in 
+#' ((\url{http://resource.psychencode.org/Datasets/Derived/DER-03a_hg19_PEC_enhancers.bed}))
+#' 
+"enhancers"
